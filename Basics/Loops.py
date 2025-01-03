c6d069a76e67ba165bd436dfe371b06cd5a0edf7
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-03 21:26:56 +0530

    added loops.py

--- Basics/Loops.py
+++ Basics/Loops.py
@@ -6,3 +6,13 @@ sum=0
 for i in str(number):
     sum+=int(i)
 print(sum) 
+
+
+def sum_of_digits(number):
+    total=0
+    for i in str(number):
+        total += int(i)
+        return total
+num= int(input("Enter a number"))
+result= sum_of_digits(num)
+print(f"The total sum is :{result}")
