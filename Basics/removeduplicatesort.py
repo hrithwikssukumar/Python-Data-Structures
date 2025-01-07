8cd76ac995939683cca3553e84ea5c4f08e23305
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-07 20:40:15 +0530

    added textreverse

---
+++ Basics/removeduplicatesort.py
@@ -0,0 +1,3 @@
+
+def remove_duplicate(text):
+    
---
+++ Basics/textreverse.py
@@ -0,0 +1,12 @@
+
+
+def text_reverse(text):
+    words = text.split()
+    reversed_list = []
+    for word in words:
+        reversed_words = word[::-1]
+        reversed_list.append(reversed_words)
+        return ' '.join(reversed_list)
+text =input("Enter the text: ")
+result = text_reverse(text)
+print(f"The reversed text is : {result}")        
