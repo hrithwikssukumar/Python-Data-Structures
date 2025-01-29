eed6dd7ced8b6d8ed7a3d2986f3d8c8c2f5a5e7b
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-29 21:40:17 +0530

    updated file

--- Basics/removeduplicatesort.py
+++ Basics/removeduplicatesort.py
@@ -6,4 +6,23 @@ def process_words(text):
 input_text = input("Enter the text: ")
 result =process_words(input_text)
 print(f"The arranged text is : {result}")    
+
+
+#class based method
+
+class TextProcessor:
+    def __init__(self, text):
+        self.text = text
+
+    def process_words(self):
+        words = self.text.split()
+        processed_words = sorted(set(words))
+        return ' '.join(processed_words)
+
+# User input and result
+input_text = input("Enter the text: ")
+processor = TextProcessor(input_text)
+result = processor.process_words()
+print(f"The arranged text is: {result}")
+
     
