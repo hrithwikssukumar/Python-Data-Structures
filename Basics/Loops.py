5bab33f11ca64d4adc3e5c20f4bd41eb9a80f433
Hrithwik <hrithwikssukumar@gmail.com>
2025-02-19 21:33:06 +0530

    updated loops.py

--- Basics/Loops.py
+++ Basics/Loops.py
@@ -16,3 +16,15 @@ def sum_of_digits(number):
 num= int(input("Enter a number"))
 result= sum_of_digits(num)
 print(f"The total sum is :{result}")
+
+
+
+
+
+
+a = [7,3,8,1,4]
+for i in range(len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]< a[j]:
+            a[i],a[j]=a[j],a[i]
+print(a)  
